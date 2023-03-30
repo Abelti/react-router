9d6dd9e4db6a4b1b2d67f77623698596fd86962f
Abelti <abeltibbeshana@gmail.com>
2023-03-30 12:53:05 +0300

    MemoryRouter

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter, MemoryRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <BrowserRouter>
       <App />
     </BrowserRouter> */}
-    <HashRouter>
+    <MemoryRouter>
       <App/>
-    </HashRouter>
+    </MemoryRouter>
   </React.StrictMode>
 );
 
